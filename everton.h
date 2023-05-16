bfb0ef4a87ecc55e19bc67d2bb6a168fe753dee1
Everton Lima <128875215+evertonbrunolima@users.noreply.github.com>
2023-05-15 21:28:01 -0300

    Update everton.h
    adicionando o exercicio 1 de arquivo

--- everton.h
+++ everton.h
@@ -1,10 +1,10 @@
-//biblioteca criada para adicionar os exercicios em 1 arquivo só
 #ifndef ARQUIVOEVERTON_H
 #define ARQUIVOEVERTON_H
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+//escrever 10 numeros aleatorios em um arquivo
 static void escrever_num() {
 		srand(time(0));
 		FILE *arq;
@@ -20,29 +20,49 @@ static void escrever_num() {
 		}
 		fclose(arq);
 }
+
+/*1. Crie um programa que obtenha o nome do arquivo
+Pergunte se irá ler ou gravar o arquivo
+Se for gravar, pergunte quantas repetições fará, e duas frases
+Passar essas informações para uma função
+Se o arquivo já existir, sobrescrever
+Se for ler, demonstrar em tela todas as linhas do arquivo*/
+
 static void arquivo_1() {
 	FILE *arq;
 	char nome[80];
 	char frase[80];
-	int op;
+	int op, l;
 	printf("digite o nome do arquivo: ");
 	fflush(stdin);
 	gets(nome);
-	printf("\ndigite a função: \n 1- ler \n 2- escrever");
-	if (op == 1) {
+	printf("\ndigite a função: \n 1- ler \n 2- escrever\n");
+	scanf("\n%d",&op);
+	
+	if (op == 1) {	
 		if ((arq=fopen(nome,"r+"))==NULL){
 			printf("achou quer ia abrir o arquivo?\nachou errado otario\n");
 			system("pause");
-		exit(1);
-		}
-		//while (fscanf(arq, "%d", &l) != EOF) {
-		//	fgets(frase,79,arq);
-		//	printf("%d %s",l,frase);
-		//}
+			exit(1);
+		}	
+		while (fscanf(arq, "%c", &l) != EOF){// leio do arquivo verificando EOF
+		fgets(frase,79,arq);
+		printf("\t%s", frase);
 	}
-	fclose(arq);
-	
-	
+	}	
+	if (op == 2) {
+		if ((arq=fopen(nome,"w"))==NULL){
+			printf("achou quer ia abrir o arquivo?\nachou errado otario\n");
+			system("pause");
+			exit(1);
+		}
+		printf("digite uma frase: ");
+		fflush(stdin);
+		gets(frase);
+		fprintf(arq,"%s",frase);
+		
+	}
+	fclose(arq);	
 }
 
 #endif
