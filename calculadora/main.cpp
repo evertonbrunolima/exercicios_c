f17b712736fcf5de12ec0073be5d22a028dfefac
Everton Lima <128875215+evertonbrunolima@users.noreply.github.com>
2023-08-15 18:49:39 -0300

    Create main.cpp

---
+++ calculadora/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	cout << "-----------------------------------------\n";
+	cout << "|        Bem vindo a calculadora        |\n";
+	cout << "-----------------------------------------\n";
+	cout << "|  Menu:                                |\n";
+	cout << "|  1- adicao                            |\n";
+	cout << "|  2- substracao                        |\n";
+	cout << "|  3- multiplicacao                     | \n";
+	cout << "|  4- divisao                           | \n";
+	cout << "|  5- potencia                          | \n";
+	cout << "|  6- raiz                              | \n";
+	cout << "|  7- eh_par                            | \n";
+	cout << "|  8- eh_impar                          | \n";
+	cout << "|  9- eh_primo                          | \n";
+	cout << "|  0- sair                              | \n";
+	cout << "-----------------------------------------\n";
+	cout << "Digite uma opcao:";
+	int opcao;
+	cin >> opcao; 
+	switch (opcao){
+		case 
+	}
+
+	
+	
+}
