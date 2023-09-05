6de8142accbdbacdc43c703703e5a36e87e863fe
Everton Lima <128875215+evertonbrunolima@users.noreply.github.com>
2023-09-05 19:09:34 -0300

    Update lista.cpp

--- Lista_encadeada_simples/lista.cpp
+++ Lista_encadeada_simples/lista.cpp
@@ -1,1 +1,29 @@
+#include "lista.h"
 
+Lista* inicializa(){
+	return NULL;
+}
+
+Lista* insere_inicio(lista* l, int i){
+	Lista* novo = (Lista*) malloc (sizeof(Lista));
+	novo->info = i; 
+	novo->prox = l;
+	return novo;
+}
+
+void imprime (Lista* l){
+	if (!vazia(l)){
+		Lista* p = l; 
+		for (; p != NULL; p=p->prox){
+		cout << "info =" << p->info << "\n";
+		}
+	}
+	else {
+		cout << "Lista eh vazia!";
+	}
+	
+}
+
+bool vazia(lista* l){
+	return (l==NULL);
+}
