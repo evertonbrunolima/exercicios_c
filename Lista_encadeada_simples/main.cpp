d7ee9d1be150a9dbc15dacdd6150920a32c44d6b
Everton Lima <128875215+evertonbrunolima@users.noreply.github.com>
2023-09-05 19:10:14 -0300

    Update main.cpp

--- Lista_encadeada_simples/main.cpp
+++ Lista_encadeada_simples/main.cpp
@@ -1,1 +1,19 @@
+#include "lista.h"
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	Lista *l; 
+	l = inicializa();
+	l = insere_inicio(l,1); 
+	l = insere_inicio(l,2); 
+	l = insere_inicio(l,3); 
+	l = insere_inicio(l,4); 
+	l = insere_inicio(l,5); 
+	l = insere_inicio(l,6); 
+	imprime (l);
+	system("pause");
+	return 0;
+}
+
 
