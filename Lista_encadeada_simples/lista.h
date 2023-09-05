75287010f0902ac26a6ac29f0d09382ca5a032e7
Everton Lima <128875215+evertonbrunolima@users.noreply.github.com>
2023-09-05 19:09:53 -0300

    Update lista.h

--- Lista_encadeada_simples/lista.h
+++ Lista_encadeada_simples/lista.h
@@ -1,1 +1,15 @@
+#include <iostream>
+#include <stdlib.h>
+using namespace std; 
 
+
+struct lista {
+	int info; //campo de dado do nó-dado
+	struct lista* prox; // ponteiro ao proximo no-dado
+};
+typedef struct lista Lista; 
+
+Lista* inicializa();
+void imprime (Lista* l); 
+bool vazia(lista* l);
+Lista* insere_inicio(lista *l, int i);
